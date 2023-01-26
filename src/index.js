a0296a05dfdd01a7eaa1e0170325c0250f13f131
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-26 20:17:28 +0530

    add and remove btn

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import './styles/main.scss';
 
-const dataBase = [
+const dataBaseDefault = [
   {
-    checked: true,
+    check: true,
     task: 'stop Procrastination',
     id: new Date('December 17, 1995 03:24:00'),
   },
   {
-    checked: false,
+    check: false,
     task: 'stop Procrastination',
     id: new Date('December 17, 1995 03:24:00'),
   },
   {
-    checked: true,
+    check: true,
     task: 'stop Procrastination',
     id: new Date('December 17, 1995 03:24:00'),
   },
 ];
+
+const dataBase = JSON.parse(localStorage.getItem('dataBase')) || dataBaseDefault;
 
 const form = document.getElementById('formId');
 const taskGen = document.getElementById('taskGen');
@@ -25,41 +27,74 @@ const tasks = document.getElementById('tasks');
 const htmlGenerator = () => {
   tasks.innerHTML = '';
   dataBase.forEach((element) => {
-    if (element.checked) {
+    if (element.check) {
       tasks.innerHTML += `
         <div class="task-list" draggable="true">
           <div>
-            <input type="checkbox" name="" id="${element.id}" checked>
-            <label for="${element.id}" contenteditable="true">${element.task}</label>
+            <input class="checkbox" type="checkbox" name="" id="${element.id}" checked>
+            <label contenteditable="true">${element.task}</label>
           </div>
           <span class="material-symbols-outlined">
             drag_indicator
           </span>
+          <i class="fa-solid fa-trash-can hidden"></i>
         </div>
     `;
     } else {
       tasks.innerHTML += `
         <div class="task-list" draggable="true">
           <div>
-            <input type="checkbox" name="" id="task-1">
-            <label for="task-1" contenteditable="true">${element.task}</label>
+            <input class="checkbox" type="checkbox" name="" id="${element.id}">
+            <label contenteditable="true">${element.task}</label>
           </div>
           <span class="material-symbols-outlined">
             drag_indicator
           </span>
+          <i class="fa-solid fa-trash-can hidden"></i>
         </div>
     `;
     }
   });
 };
 htmlGenerator();
-function submitForm(event) {
-  // Preventing page refresh
-  event.preventDefault();
-  dataBase.push({ checked: false, task: `${taskGen.value}`, id: new Date() });
-  htmlGenerator();
+
+function submitForm() {
+  dataBase.push({ check: false, task: `${taskGen.value}`, id: new Date() });
+  localStorage.setItem('dataBase', JSON.stringify(dataBase));
   taskGen.value = '';
-  return false;
 }
+
 form.addEventListener('submit', submitForm);
-// draggable and other functionalities
+
+const editAble = document.querySelectorAll('.task-list')
+const dragBTn = document.querySelectorAll('.material-symbols-outlined')
+const trash = document.querySelectorAll('.fa-trash-can')
+const checkbox = document.querySelectorAll('.checkbox')
+
+for (let i = 0; i < editAble.length; i++) {
+  editAble[i].addEventListener('mouseover', ()=>{
+    dragBTn[i].classList.toggle('hidden');
+    trash[i].classList.toggle('hidden')
+  });
+
+  editAble[i].addEventListener('mouseout', ()=>{
+    dragBTn[i].classList.toggle('hidden');
+    trash[i].classList.toggle('hidden');
+  });
+
+  trash[i].addEventListener('click', ()=>{
+    dataBase.splice(i,1);
+    localStorage.setItem('dataBase', JSON.stringify(dataBase));
+    window.location.reload()
+  })
+
+  checkbox[i].addEventListener('input',()=>{
+    if(dataBase[i].check===false) {
+      dataBase[i].check=true
+      localStorage.setItem('dataBase', JSON.stringify(dataBase));
+    }else if(dataBase[i].check===true){
+      dataBase[i].check = false
+      localStorage.setItem('dataBase', JSON.stringify(dataBase));
+    }
+  })
+}
